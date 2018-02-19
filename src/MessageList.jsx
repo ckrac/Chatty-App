d25eb4abe36a0b26cb119d742200f120a71938d5
vagrant <kennethracadio@hotmail.com>
2018-02-19 22:21:31 +0000

    4-Add unique key prop

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -3,10 +3,11 @@ import Message from './Message.jsx';
 
 class MessageList extends Component {
   render() {
+    console.log("Rendering <MessageList />");
     return (
       <main className="messages">
       {this.props.messages.map((message) => {
-        return <Message content={message.content} username={message.username} />
+        return <Message key ={message.id} content={message.content} username={message.username} />
       })}
       </main>
     );
