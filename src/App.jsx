9e1f4397404f9a0ad1e3599ec43b60b4df8f1aeb
vagrant <kennethracadio@hotmail.com>
2018-02-20 00:06:53 +0000

    Handling User Actions

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,30 @@ class App extends Component {
     }
   }
 
+  addMessage = message => {
+    console.log(message);
+    // new array by update this.state with message object
+    const newMessages = [...this.state.messages, message]
+    // this.setState({messages: newMessages})
+    // const messagesClone = this.state.messages;
+    // this.setState({messages: messagesClone.concat(message)})
+
+    this.setState({messages: newMessages})
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount <App />");
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+      // Add a new message to the list of messages in the data store
+      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+      const messages = this.state.messages.concat(newMessage)
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({messages: messages})
+    }, 3000);
+  }
+
   render() {
     return (
       <div>
@@ -29,7 +53,7 @@ class App extends Component {
         <a href="/" className="navbar-brand">Chatty</a>
       </nav>
       <MessageList messages={this.state.messages} />
-      <ChatBar name={this.state.currentUser.name} />
+      <ChatBar name={this.state.currentUser.name} add={this.addMessage}/>
       </div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,13 +1,50 @@
 import React, {Component} from 'react';
 
 class ChatBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: "",
+      username: "",
+      content: ""
+    }
+  }
+
   render() {
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder={this.props.name} />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input className="chatbar-username"
+          placeholder={this.props.name}
+          value={this.state.username}
+          onChange={this._nameChanged}
+        />
+        <input className="chatbar-message"
+          placeholder="Type a message and hit ENTER"
+          value={this.state.content}
+          onChange={this._contentChanged}
+          onKeyPress={e => {
+            if (e.key === "Enter") {
+              this._submitChange();
+            }
+          }}
+        />
       </footer>
     );
+  }
+
+  _nameChanged = e => {
+    this.setState({username: e.target.value})
+  }
+
+  _contentChanged = e => {
+    this.setState({content: e.target.value});
+  }
+
+  _submitChange = () => {
+    console.log('pressed enter');
+    console.log(this.state.username);
+    console.log(this.state.content);
+    this.props.add(this.state);
   }
 }
 
