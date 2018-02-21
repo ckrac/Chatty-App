37fa7181b2f460a1355423c34646cbdd3819dcd8
vagrant <kennethracadio@hotmail.com>
2018-02-21 20:28:08 +0000

    Add chatty server folder to proj and run socket server in root file

---
+++ chatty_server/server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const SocketServer = require('ws').Server;
+const WebSocket = require('ws');
+
+// id generator
+const uuidv4 = require('uuid/v4');
+
+// Set the port to 3001
+const PORT = 3001;
+
+// Create a new express server
+const server = express()
+   // Make the express server serve static assets (html, javascript, css) from the /public folder
+  .use(express.static('public'))
+  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+
+// Create the WebSockets server
+const wss = new SocketServer({ server });
+
+// Set up a callback that will run when a client connects to the server
+// When a client connects they are assigned a socket, represented by
+// the ws parameter in the callback.
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  let totalClients = wss.clients.size;
+  console.log("total clients", totalClients)
+  let countObj = {type: "incomingConnectionCount", totalClients : totalClients};
+
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(countObj));
+    }
+  });
+
+  // Handle incoming messages from client
+  ws.on('message', function incoming(message) {
+
+    let messageObj = JSON.parse(message)
+    const id = uuidv4();
+    messageObj.id = id;
+
+    if(messageObj.type === "postMessage") {
+      console.log(`User ${messageObj.username} said ${messageObj.content}, type = ${messageObj.type}`);
+      messageObj.type = "incomingMessage";
+
+      wss.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify(messageObj));
+        }
+      });
+
+    } else  if (messageObj.type === "postNotification"){
+      console.log(`Current user: ${messageObj.user}, type = ${messageObj.type}`)
+      messageObj.type = "incomingNotification";
+
+      wss.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify(messageObj));
+        }
+      });
+    }
+
+  })
+
+  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
+  ws.on('close', () => {
+    console.log('Client disconnected');
+    let count =  wss.clients.size;
+    let decreaseCount = {type: "incomingUpdateConnection", totalClients : count};
+    wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(decreaseCount));
+      }
+    });
+
+  });
+
+
+});
